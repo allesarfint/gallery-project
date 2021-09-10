c2540ba4d31dac8dd682191cf3355418b363b339
allesarfint <81056093+allesarfint@users.noreply.github.com>
2021-09-09 22:04:49 -0500

    Fixing the mess

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: 'development',
@@ -10,15 +11,24 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin ({
+            title: 'Restaurant Page',
+            filename: 'index.html',
+            template: './src/index.html',
+        }),
+    ],
     module: {
         rules: [
             {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                test: /\.(s[ac]ss|css)$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
             },
             {
-                test: /\.jpg$/i,
+                test: /\.(jpg|mp4|gif|png)$/i,
                 type: 'asset/resource',
             },
         ],
