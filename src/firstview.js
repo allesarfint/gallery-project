3a9a904fd0a283b666c13feb2ac76483bbf89d91
Desfint <allesarfint@gmail.com>
2021-09-10 02:06:35 -0500

    Firstview arrow and text added

--- src/firstview.js
+++ src/firstview.js
@@ -13,17 +13,66 @@ export function createView() {
     projectTitle.classList.add("project");
     viewContent.appendChild(projectTitle);
 
+    const closeButton = document.createElement("span");
+    closeButton.textContent = "×";
+    const afterText = document.createElement("article");
+    afterText.id = "after-text";
+    afterText.style.cursor = "pointer";
+    const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas elit a ligula condimentum, vitae ultrices enim venenatis. Curabitur sit amet elementum lorem, in lobortis purus. Donec turpis nibh, imperdiet et tellus sit amet, euismod euismod sem. Praesent nunc erat, ornare sed malesuada et, varius quis arcu. Vivamus urna lacus, interdum at lacus quis, finibus vehicula dolor. In consectetur accumsan blandit. Sed id turpis vitae eros feugiat aliquam id nec massa. Sed vitae faucibus justo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque bibendum gravida neque.`;
+    afterText.textContent = text;
+
     const animatedPoint = document.createElement("div");
     animatedPoint.classList.add("animated-point");
     animatedPoint.addEventListener("click", function () {
-        view.style.animationName = "fadeout";
-        view.style.animationDuration = "0.6s";
+        animatedPoint.style.animationName = "fadeout";
+        animatedPoint.style.animationDuration = "1s";
         setTimeout (function () {
-            view.parentNode.removeChild(view);
-        }, 500)
+            viewContent.removeChild(animatedPoint);            
+        }, 990)
+        projectTitle.style.animationName = "fadeout";
+        projectTitle.style.animationDuration = "1s";
+        setTimeout (function () {
+            projectTitle.style.animationName = "fadein";
+            projectTitle.style.animationDuration = "1s";     
+        }, 1000)
+        arrow.style.animationName = "fadeout";
+        arrow.style.animationDuration = "1s";
+        setTimeout (function () {
+            viewContent.removeChild(arrow);
+            viewContent.appendChild(afterText);
+            viewContent.appendChild(arrow);
+            afterText.style.animationName = "fadein";
+            afterText.style.animationDuration = "1s";
+        }, 1000)
+        setTimeout (function () {
+            arrow.style.animationName = "fadein";
+            arrow.style.animationDuration = "1s";     
+        }, 1000)
             
     })
     viewContent.appendChild(animatedPoint);
+
+    setTimeout(function () {
+        afterText.addEventListener("click", function() {
+            viewContent.style.animationName = "fadeout";
+            viewContent.style.animationDuration = "1s";
+
+            setTimeout(function() {
+                view.removeChild(viewContent);
+                view.style.animationName = "fadeout";
+                view.style.animationDuration = "1s";
+                setTimeout (function () {
+                    view.parentNode.removeChild(view);
+                }, 990)
+            }, 990)
+        })
+    }, 2000)
+
+    const arrow = document.createElement("img");
+    arrow.src = "http://a1sites.com/FreeImages/Free_Clip_Art/images/arrow/up_arrow_clip.png";
+    arrow.style.width = "20px";
+    arrow.style.marginTop = "20px";
+    viewContent.appendChild(arrow);
 
     return view
 }
